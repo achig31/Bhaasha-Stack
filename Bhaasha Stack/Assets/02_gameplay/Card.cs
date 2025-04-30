aa6998b6b7fd00fe628f132452ce5ea8131e6cec
Anoushka <anoushka.chig@gmail.com>
2025-04-30 16:12:49 -0500

    animations (failed)

--- Bhaasha Stack/Assets/02_gameplay/Card.cs
+++ Bhaasha Stack/Assets/02_gameplay/Card.cs
@@ -24,6 +24,7 @@ public class Card : MonoBehaviour
             isFlipped = true;
             cardImage.sprite = gameManager.cardFaces[cardID];
             gameManager.CardFlipped(this);
+
         }
     }
 
